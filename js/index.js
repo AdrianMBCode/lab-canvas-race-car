d445ff0ed5383cbe7d614c4e05d2d635a366e4a3
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-01-26 22:36:15 +0100

    class Obstacle creada

--- js/index.js
+++ js/index.js
@@ -2,30 +2,36 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
 
-let imageRoad
-let imageCar
+  let imageRoad;
+  let imageCar;
+  let obstacles = [];
+
   function startGame() {
     imageRoad = new Image();
     imageRoad.src = "images/road.png";
     imageRoad.addEventListener('load', () => {
-    ctx.drawImage(imageRoad, 0, 0, 500, 700);
-    })
+      ctx.drawImage(imageRoad, 0, 0, 500, 700);
+    });
+
     imageCar = new Image();
     imageCar.src = "images/car.png";
     imageCar.addEventListener('load', () => {
-    ctx.drawImage(imageCar, 213, 550, 70, 125)
-    })
-  
+      ctx.drawImage(imageCar, 213, 550, 70, 125);
+    });
 
     setInterval(() => {
       update();
-    },20);
+    }, 20);
+
+    setInterval(() => {
+      createObstacle();
+    }, 1000);
   }
 
-class Car {
+  class Car {
     constructor(x, velocidadx) {
       this.x = x;
       this.velocidadx = velocidadx;
@@ -33,30 +39,43 @@ class Car {
     }
     print() {
       ctx.drawImage(imageCar, this.x, 550, this.width, 125);
-  
     }
   }
-  let cars = new Car(213, 20)
+
+  class Obstacle {
+    constructor(x) {
+      this.x = x;
+      this.y = 0;
+      this.width = 30;
+      this.height = 40;
+    }
+    print() {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    update() {
+      this.y += 5;
+    }
+  }
+
+  let car = new Car(213, 20);
   let update = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(imageRoad, 0, 0, 500, 700);
-    cars.print();
-  }
-  
+    car.print();
 
-   
+  };
 
   document.getElementsByTagName("body")[0].addEventListener("keydown", (event) => {
     switch (event.key) {
       case "ArrowLeft":
-        if (cars.x > 0) cars.x -= cars.velocidadx;
+        if (car.x > 0) car.x -= car.velocidadx;
         break;
 
       case "ArrowRight":
-        if (cars.x + cars.width < canvas.width) cars.x += cars.velocidadx;
+        if (car.x + car.width < canvas.width) car.x += car.velocidadx;
         break;
       default:
       }
-    })  
+    });
   }
 
